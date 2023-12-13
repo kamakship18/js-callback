381e9d96892ce6b0017935f1184408290f617133
kamakship18 <kamakshipandoh8@gmail.com>
2023-12-13 09:30:36 +0530

    completed js

--- callback.js
+++ callback.js
@@ -6,8 +6,25 @@ const newCookie = {name:"Biscotti Cookies"};
 
   // Progression 2: using setTimeout() 
 
-//Progression 3: Create a function to creat cookies
+  function getCookies() {
+    setTimeout(() => {
+      let result = '';
+      cookies.forEach((cookie) => {
+        result += `<li>${cookie.name}</li>`;
+      });
+      document.body.innerHTML = result;
+    }, 1000);
+  }
 
+  getCookies()
+//Progression 3: Create a function to creat cookies
+function createCookie(newCookie, callback) {
+  setTimeout(() => {
+    cookies.push(newCookie);
+    callback();
+  },2000)
+}
 
 
 // Progression 5: calling function
+createCookie(newCookie, getCookies);
